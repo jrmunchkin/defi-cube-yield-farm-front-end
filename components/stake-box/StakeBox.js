628d6fa28aedb90da722fcca895f20ba27b86d27
jdeveauxmindflurry <jeanremi.developer@gmail.com>
2023-03-02 15:36:52 +0800

    Add responsive

--- components/stake-box/StakeBox.js
+++ components/stake-box/StakeBox.js
@@ -7,13 +7,13 @@ export default function StakeBox({ supportedTokens, cubeFarmAddress }) {
   return (
     <div className="flex items-center flex-col p-8">
       <h1 className="text-white text-2xl p-4"> Stake your tokens! </h1>
-      <div className="box-border rounded-lg p-4 border-2 bg-white">
-        <TabList defaultActiveKey={0} tabStyle="bar">
+      <div className="box-border w-350 rounded-lg border-2 p-5 bg-white">
+        <TabList defaultActiveKey={0} tabStyle="bulbSeperate">
           {supportedTokens.map((token, index) => {
             return (
               <Tab tabKey={index} tabName={token.name}>
                 <div className="flex items-center flex-col">
-                  <div className="grid gap-4 grid-cols-2 grid-rows-1">
+                  <div className="grid gap-4 grid-cols-2 mt-4 grid-rows-1">
                     <UnstakedBalance token={token} />
                     <StakedBalance
                       token={token}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        350: "350px",
+      },
+    },
   },
   plugins: [],
 };
